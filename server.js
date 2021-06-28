677c00b3febabe275b086bbaba566734c7f42ea2
chagy <chagy_x_@hotmail.com>
2021-06-28 16:10:43 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 8080;
+
+//log requests
+app.use(morgan("tiny"));
+
+//parse request to body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+// app.set("view",path.resolve(__dirname,"views/ejs"));
+
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+app.get("/", (req, res) => {
+  res.sender("index");
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:${3000}`);
+});
