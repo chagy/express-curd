ff6c6ad32d4a47cff0593cece5cf4d6a4b935ca3
chagy <chagy_x_@hotmail.com>
2021-07-01 16:14:58 +0700

    1:32:08

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const connectDB = require("./server/database/connection");
+
 const app = express();
 
 dotenv.config({ path: "config.env" });
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 8080;
 
 //log requests
 app.use(morgan("tiny"));
+
+connectDB();
 
 //parse request to body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,8 +28,18 @@ app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 app.get("/", (req, res) => {
-  res.sender("index");
+  res.render("index");
 });
+
+app.get("/add-user", (req, res) => {
+  res.render("add_user");
+});
+
+app.get("/update-user", (req, res) => {
+  res.render("update_user");
+});
+
+app.use("/", require("./server/routes/router"));
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:${3000}`);
