43a985e11450548591796f788b4ff86627b62a92
chagy <chagy_x_@hotmail.com>
2021-07-13 15:37:24 +0700

    complete

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,16 @@
+const axios = require("axios");
+
 exports.homeRoutes = (req, res) => {
-  res.render("index");
+  console.log("test");
+  axios
+    .get(`http://localhost:3000/api/users`)
+    .then(function (response) {
+      console.log(response);
+      res.render("index", { users: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 exports.add_user = (req, res) => {
